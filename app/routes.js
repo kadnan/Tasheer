fb971caa063af96fc48969e848eff39bf8415ec8
Adnan Siddiqi <kadnan@gmail.com>
2017-02-05 00:44:46 +0500

    View with Variable and MySQL Connection

--- app/routes.js
+++ app/routes.js
@@ -7,8 +7,30 @@ var router = express.Router();
 
 module.exports = router;
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname,'../index.html'))
-    res.render('pages/index')
+    jobs = []
+    jobs.push(
+        {
+            'category_name': 'Programming',
+            'jobs': [
+                {
+                    'id': 1,
+                    'title': 'Senior Java Developer - Security Cleared - Java 8 - Contract',
+                    'category_id': 1,
+                    'company_name': 'Oracle',
+                    'created_at': '2017-02-09'
+                }],
+
+        });
+    //Query Jobs with Categories
+    var query = "   SELECT category_id,title,name AS category_name FROM posts p \
+                    INNER JOIN categories c \
+                    ON p.category_id = c.id \
+                    ORDER BY category_id";
+
+
+    res.sendFile(path.join(__dirname, '../index.html'))
+
+    res.render('pages/index', {'jobs': jobs})
 })
 
 router.get('/about', function (req, res) {
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+
+
 var express = require('express');
+var mysql = require('mysql');
 var expressLayouts = require('express-ejs-layouts');
 var app = express();
 var port = 3000;
 
+/**
+ * Making MySQL Connection
+ */
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'Tasheer'
+});
+connection.connect(null, function () {
+    console.log('Connected Successfully')
+    isDbConnected = true;
+})
+
 // use ejs and express Layouts
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(expressLayouts)
 
 var router = require('./app/routes');
